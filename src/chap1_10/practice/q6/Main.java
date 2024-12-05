2c5bc10ce12afa427f12bd51eb80c15beffaa211
soongu <naong526@naver.com>
2024-12-05 09:30:05 +0900

    인터페이스 연습문제

---
+++ src/chap1_10/practice/q6/Main.java
@@ -0,0 +1,13 @@
+package chap1_10.practice.q6;
+
+public class Main {
+    public static void main(String[] args) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        mediaPlayer.addMedia(new AudioPlayer());
+        mediaPlayer.addMedia(new VideoPlayer());
+        mediaPlayer.addMedia(new ImageDisplay());
+
+        mediaPlayer.playAll();
+    }
+}
---
+++ src/chap1_10/practice/q6/MediaPlayable.java
@@ -0,0 +1,7 @@
+package chap1_10.practice.q6;
+
+public interface MediaPlayable {
+    void play();
+    void pause();
+    void stop();
+}
